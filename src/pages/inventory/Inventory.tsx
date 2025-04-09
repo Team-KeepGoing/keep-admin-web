e69d2f26478ecd0358ebd960bed9b95badd5d1a7
소진 <136438067+sjsojin@users.noreply.github.com>
2025-04-09 09:16:32 +0900

    ⚡ :: table 기능 추가

--- src/pages/inventory/Inventory.tsx
+++ src/pages/inventory/Inventory.tsx
@@ -1,14 +1,22 @@
 import * as S from './style';
-import React from 'react';
+import React, { useState } from 'react';
 import Navbar from '@/components/navbar/Navbar';
-import DeviceTable from '@/components/deviceTable/DeviceTable';
+import DeviceTable from '@/components/deviceTable2/DeviceTable2';
+import ItemDetailCard from '@/components/itemDetailCard/ItemDetailCard';
 
 const Inventory = () => {
+  const [selectedItemId, setSelectedItemId] = useState<number | null>(null);
+
   return (
     <S.page_container>
       <Navbar />
       <S.title>관리 물품 현황</S.title>
-      <DeviceTable />
+      <S.Div>
+        <DeviceTable onSelectItem={setSelectedItemId} />
+      </S.Div>
+      <S.Div2>
+        {selectedItemId && <ItemDetailCard id={selectedItemId} />}
+      </S.Div2>
     </S.page_container>
   );
 };
